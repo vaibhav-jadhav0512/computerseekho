f40c49786f7176e1f3d14548ff506cae4051164c
TejasPatil88 <tejaspatil76@gmail.com>
2021-03-17 19:19:27 +0530

    Payment Type Matser

---
+++ routes/PaymentTypeMaster.js
@@ -0,0 +1,15 @@
+const express =require('express');
+
+const paymentTypeMasterCtrl = require('../controllers/PaymentTypeMaster');
+
+const router = express.Router();
+
+
+router.get('/allPaymentTypeMaster',paymentTypeMasterCtrl.getAllPaymentTypeMaster);
+router.get('/PaymentTypeMaster',paymentTypeMasterCtrl.getPaymentTypeMaster);
+router.post('/addPaymentTypeMaster',paymentTypeMasterCtrl.post);
+router.delete('/deletePaymentTypeMaster',paymentTypeMasterCtrl.delete);
+router.patch('/updatePaymentTypeMaster',paymentTypeMasterCtrl.patch);
+
+
+module.exports = router;
